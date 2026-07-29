4ab8f9fbf803f5bed4cf78e5e7524cc9248bccd7
fathirezi12 <fathirezi12@gmail.com>
2026-07-30 01:40:19 +0800

    Update produk.js

--- js/produk.js
+++ js/produk.js
@@ -1,1 +1,18 @@
 
+let barang = JSON.parse(localStorage.getItem("barang")) || [];
+
+let list = document.getElementById("listProduk");
+
+if (barang.length === 0) {
+    list.innerHTML = "<p>Belum ada produk.</p>";
+} else {
+    barang.forEach((item) => {
+        list.innerHTML += `
+        <div style="background:white;padding:15px;margin-bottom:10px;border-radius:10px;">
+            <h3>${item.nama}</h3>
+            <p>Harga: Rp${item.harga}</p>
+            <p>Stok: ${item.stok}</p>
+        </div>
+        `;
+    });
+}
