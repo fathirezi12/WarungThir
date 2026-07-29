ccaf5cf9d5761fa51acf5e9e35e5877e36ee1219
fathirezi12 <fathirezi12@gmail.com>
2026-07-30 01:04:40 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-console.log("Selamat datang di FATHIRPOS");
+function login(){
 
-alert("FATHIRPOS berhasil dijalankan!");
+    let username =
+        document.getElementById("username").value;
+
+    let password =
+        document.getElementById("password").value;
+
+    if(username=="admin" && password=="1234"){
+
+        alert("Login berhasil!");
+
+    }else{
+
+        alert("Username atau Password salah!");
+
+    }
+
+}
