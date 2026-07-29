d0ac154afd815a399a6980d6767f744d4633dbfd
fathirezi12 <fathirezi12@gmail.com>
2026-07-30 01:32:05 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,3 +8,26 @@ function login() {
         alert("Username atau Password salah!");
     }
 }
+function simpanBarang() {
+
+let nama = document.getElementById("nama").value;
+let harga = document.getElementById("harga").value;
+let stok = document.getElementById("stok").value;
+
+let barang = JSON.parse(localStorage.getItem("barang")) || [];
+
+barang.push({
+    nama: nama,
+    harga: harga,
+    stok: stok
+});
+
+localStorage.setItem("barang", JSON.stringify(barang));
+
+alert("Barang berhasil disimpan!");
+
+document.getElementById("nama").value = "";
+document.getElementById("harga").value = "";
+document.getElementById("stok").value = "";
+
+}
